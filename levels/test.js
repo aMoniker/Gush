cec52a22c07d6281358140151b6bd3f6e0ce306f
James Greenleaf <>
2021-08-04 00:12:51 +0000

    add level legend TODO list

--- levels/test.js
+++ levels/test.js
@@ -1,19 +1,52 @@
 import { k } from "/kaboom.js"
 import { floorTileRandom, floorLadder, floorTrap } from "/objects/floor.js";
 
-// const characters = {
-//     "a": {
-//         sprite: ,
-//         msg: "ohhi how are you",
-//     },
-// };
+/**
+ * To implement:
+ * + door
+ * > hole/down
+ * < ladder/up
+ * [ top-left corner wall
+ * - top wall
+ * ] top-right corner wall
+ * | left/right wall
+ * { bottom-left corner wall
+ * _ bottom wall
+ * } bottom-right corner wall
+ * & red fountain (basin should render below? also top tile?)
+ * % blue fountain (see above)
+ * ! goo fountain (see above)
+ * ; wall banner (maybe same color for whole level
+ * 
+ * Special handling:
+ * probably have to render tiles underneath on first pass,
+ * then on second pass spawn objects with level.getPos()
+ * ? chest
+ * $ coin
+ * # crate
+ * @ player spawn
+ * d small demon
+ * D big demon
+ * f random flask
+ * g goblin
+ * i imp
+ * m muddy
+ * n necromancer
+ * o ogre
+ * O random orc
+ * s skeleton
+ * S swampy
+ * w wogol
+ * Z big zombie
+ * z random zombie
+ */
 
 export const loadLevel = () => {
 
   let tileCounter = 0;
 
   const map = [
-    "[------------------]",
+    "[-------&----------]",
     "|..................|",
     "|..................|",
     "|...........^......|",
@@ -30,84 +63,29 @@ export const loadLevel = () => {
     const x = tileIndex % mapWidth;
     const y = Math.floor(tileIndex / mapWidth);
     return {
-      up: map?.[y-1]?.[x],
-      down: map?.[y+1]?.[x],
-      left: map?.[y]?.[x-1],
-      right: map?.[y]?.[x+1],
+      up: map ?.[y - 1] ?.[x],
+      down: map ?.[y + 1] ?.[x],
+      left: map ?.[y] ?.[x - 1],
+      right: map ?.[y] ?.[x + 1],
     }
   };
 
   const level = k.addLevel(map, {
-      width: 16,
-      height: 16,
-      pos: k.vec2(0, 0),
-      // ".": floorTileRandom,
-      // "*": [
-      //   k.sprite("dungeon", { frame: 34 }),
-      //   k.solid(),
-      //   k.layer("game"),
-      //   "block",
-      // ],
-      // use a callback for dynamic evauations per block
-      // "?": () => { // chest
-      //   return [
-      //     k.sprite("dungeon", { frame: 35 }),
-      //     k.color(0, 1, k.rand(0, 1)),
-      //     k.layer("game"),
-      //     "block",
-      //   ];
-      // },
-      // "<": floorLadder,
-      // ">": hole?,
-      // "^": floorTrap,
-      // any catches anything that's not defined by the mappings above, good for more dynamic stuff like this
-      any(tile) {
-        // console.log("tile", tileCounter, tile);
-        
+    width: 16,
+    height: 16,
+    pos: k.vec2(0, 0),
+    // any is used for all tiles so we can keep count and find neighbors
+    any(tile) {
+      const neighbors = getNeighborTiles(tileCounter);
+      tileCounter++;
 
-        const neighbors = getNeighborTiles(tileCounter);
-        // console.log("neighbors", neighbors);
+      if (tile === ".") {
+        return floorTileRandom();
+      } else if (tile === "^") {
+        return floorTrap();
+      }
+    },
+  });
 
-        tileCounter++;
-
-
-        // let tileObject = [];
-        if (tile === ".") {
-          return floorTileRandom();
-        } else if (tile === "^") {
-          return floorTrap();
-        }
-
-
-        // return [];
-        // if (characters[ch]) {
-        //     return [
-        //         sprite(char.sprite),
-        //         solid(),
-        //         "character",
-        //         {
-        //             msg: characters[ch],
-        //         },
-        //     ];
-        // }
-        // tileCounter++;
-        // return tileObject;
-      },
-    });
-    return level;
+  return level;
 }
-
-// query size
-// map.width();
-// map.height();
-
-// get screen pos through map index
-// map.getPos(x, y);
-
-// destroy all
-// map.destroy();
-
-// there's no spatial hashing yet, if too many blocks causing lag, consider hard disabling collision resolution from blocks far away by turning off 'solid'
-// action("block", (b) => {
-//     b.solid = player.pos.dist(b.pos) <= 20;
-// });
